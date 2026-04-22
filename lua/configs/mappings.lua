9623a4d9fd7b90b12da143e66d60ddd0885dc05c
mxndx-mxn <armando.san.2006@gmail.com>
2026-04-22 09:13:30 -0500

    My neovim config

---
+++ lua/configs/mappings.lua
@@ -0,0 +1,7 @@
+M.vimtex = {
+    n = {
+        ["<leader>ll"] = { "<cmd>VimtexCompile<cr>", "Toggle LaTeX Compile" },
+        ["<leader>lv"] = { "<cmd>VimtexView<cr>", "View PDF" },
+        ["<leader>le"] = { "<cmd>VimtexErrors<cr>", "Show TeX Errors" },
+    },
+}
